694e34e297d73c5492f46e1b865e58a64bfb4d65
unknown <dookiewiz@disroot.org>
2026-07-31 18:40:06 +0200

    now timer should adjust itself to the time offset

--- audit_godot/main.gd
+++ audit_godot/main.gd
@@ -2,42 +2,51 @@ extends Node
 
 @onready var debug_label = %Debug
 
-var wait_t : int = 10;
+var wait_t : int 
 var time_dict = Time.get_time_dict_from_system()
 func _ready():
-	%Timer.set_wait_time(wait_t)
-	%Timer.start()
+	#%Timer.set_wait_time(wait_t)
+	#%Timer.start()
 	$Sound.volume_db = 8
 	
  
 
 func _on_timer_timeout() -> void:
-	#%Sound.play()
+	
+	%Sound.play()
+	
+	%debug_label.text = "went off at"
+	time_dict = Time.get_time_dict_from_system()
+	%show_time.text = str(time_dict.hour, ":", time_dict.minute, ":", time_dict.second)
+	# readjust the timer once it goes of at hour:00:00
+	wait_t = 60*60
+	%Timer.set_wait_time(wait_t)
 	pass
 
 #	Stopp Button
 func _on_stopp_button_up() -> void:
-	debug_label.text = str(sec/60)
+	
 	%Sound.stop()
 
-	
-#	print(time_dict.hour, ":", time_dict.minute, ":", time_dict.second)
-#	print(time_dict.minute)
-	print(time_dict.second)
-var ad_t : int 
-var sec : int
-func _process(delta: float) -> void:
-	time_dict = Time.get_time_dict_from_system()
-	%show_time.text = str(time_dict.hour, ":", time_dict.minute, ":", time_dict.second)
-	if time_dict.second == 0:
-		%Sound.play()
-	#	Show Time in seconds
-	#ad_t += 1
-	#if (ad_t % 165) == 0:
-		#sec += 1
-		#%show_time.text = str(sec)
-	
 
+var wait_sec : int
+var wait_minute : int
+var timer_set : bool = false
+func _process(delta: float) -> void:
+	if timer_set == false : 
+		time_dict = Time.get_time_dict_from_system()
+		%show_time.text = str(time_dict.hour, ":", time_dict.minute, ":", time_dict.second)
+		wait_sec = 60 - time_dict.second
+		wait_minute = 60 - time_dict.minute
+		if wait_sec == 60:
+			wait_sec = 0
+		if wait_minute == 60 :
+			wait_minute = 0
+		wait_t = wait_sec * 60 + wait_minute
+		%Timer.set_wait_time(wait_t)
+		%Timer.start() 
+		print(wait_minute, ":", wait_sec )
+		timer_set = true
 
 func _on_button_button_up() -> void:
 	pass # Replace with function body.
